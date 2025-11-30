cf6ace1f4ee357cc80eb3d89d139243d19432b93
xieck13 <xieck13@gmail.com>
2025-11-30 15:22:23 +0800

    fix: use export

--- examples/dedup_wcc/config.sh
+++ examples/dedup_wcc/config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # dataset parameters
-INPUT_PATH="/work/projects/polyullm/congkai/pretrain_data/Nemotron-CC-Math-v1/*/*.parquet"
-OUTPUT_PATH="/work/projects/polyullm/wtf/data/Nemotraon-CC-Math-v1-dedup"
-FILE_TYPE="parquet"
-TEXT_KEY="text"
+export INPUT_PATH="/work/projects/polyullm/congkai/pretrain_data/Nemotron-CC-Math-v1/*/*.parquet"
+export OUTPUT_PATH="/work/projects/polyullm/wtf/data/Nemotraon-CC-Math-v1-dedup"
+export FILE_TYPE="parquet"
+export TEXT_KEY="text"
 
 # dedup parameters
-THRESHOLD="0.85"
-NGRAM_SIZE="5"
-MIN_LENGTH="2"
-NUM_PERM="128"
-B="8"
-R="16"
-WITH_SPLIT="False"
-RUN_CHUKONU="True"
+export THRESHOLD="0.85"
+export NGRAM_SIZE="5"
+export MIN_LENGTH="2"
+export NUM_PERM="128"
+export B="8"
+export R="16"
+export WITH_SPLIT="False"
+export RUN_CHUKONU="True"
