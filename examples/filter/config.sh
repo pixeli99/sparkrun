3e3a81871cdee767bfd5398488913c761c15a8c5
xieck13 <xieck13@gmail.com>
2025-12-01 22:48:56 +0800

    feat: more

---
+++ examples/filter/config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export INPUT_PATH="/work/projects/polyullm/congkai/pretrain_data/Nemotron-CC-Math-v1/*/*.parquet"
+export OUTPUT_PATH="/work/projects/polyullm/wtf/data/Nemotraon-CC-Math-v1-fasttext-filter"
+export FILE_TYPE="parquet"
+export TEXT_KEY="text"
