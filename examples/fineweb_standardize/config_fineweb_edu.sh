b4015edda8c1260c8b4a0249d4092267a730d43c
xieck13 <xieck13@gmail.com>
2026-01-15 18:54:25 +0800

    more

--- examples/fineweb_standardize/config_fineweb_edu.sh
+++ examples/fineweb_standardize/config_fineweb_edu.sh
@@ -5,3 +5,4 @@ export INPUT_PATH="/work/projects/polyullm/congkai/pretrain_data/fineweb-edu/100
 export OUTPUT_PATH="/work/projects/polyullm/lipengxiang_tmp/spark-runner/demo/fineweb_standardize/100BT"
 export FILE_TYPE="parquet"
 export NUM_PARTITIONS="100"
+export ADAPTER="fineweb_edu"
