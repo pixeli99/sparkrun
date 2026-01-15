78a13fae6a6a17f260ccffaed854a05390aff458
xieck13 <xieck13@gmail.com>
2026-01-15 19:27:43 +0800

    more

--- examples/fineweb_standardize/config_dclm.sh
+++ examples/fineweb_standardize/config_dclm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 输入输出路径按实际环境修改
-export INPUT_PATH="/work/projects/polyullm/congkai/pretrain_data/dclm-baseline-1.0/global-shard_03_of_10/local-shard_0_of_10/*jsonl.gz"
-export OUTPUT_PATH="/work/projects/polyullm/reallm.xyz/spark-runner/demo/dclm-baseline-1.0/shard03-00"
+export INPUT_PATH="/work/projects/polyullm/congkai/pretrain_data/dclm-baseline-1.0/global-shard_03_of_10/*/*.jsonl.gz"
+export OUTPUT_PATH="/work/projects/polyullm/reallm.xyz/spark-runner/demo/dclm-baseline-1.0/shard03"
 export FILE_TYPE="json"
-export NUM_PARTITIONS="100"
+export NUM_PARTITIONS="500"
 export ADAPTER="dclm"
