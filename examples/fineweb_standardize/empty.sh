0bf502ec000298967baf529ddbd5a38d0f25411a
xieck13 <xieck13@gmail.com>
2026-01-15 21:49:01 +0800

    more

--- examples/fineweb_standardize/empty.sh
+++ examples/fineweb_standardize/empty.sh
@@ -9,12 +9,3 @@
 
 
 # you can use this example to inject environment variables into the slurm jobs
-# INPUT_PATH="/work/projects/polyullm/congkai/pretrain_data/dolma3_mix-6T-1025/data/stack_edu-C"
-# OUTPUT_PATH="/work/projects/polyullm/reallm.xyz/spark-runner/demo/dolma3_mix-6T-1025/stack-edu-C"
-# FILE_TYPE="json"
-# NUM_PARTITIONS="100"
-# ADAPTER="dolma3_mix_code"
-
-# sbatch --reservation=megatron \
-#     --export=ALL,INPUT_PATH="$INPUT_PATH",OUTPUT_PATH="$OUTPUT_PATH",FILE_TYPE="$FILE_TYPE",NUM_PARTITIONS="$NUM_PARTITIONS",ADAPTER="$ADAPTER" \
-#     submit.sh examples/fineweb_standardize/empty.sh examples/fineweb_standardize/run_fineweb_standardize.sh
